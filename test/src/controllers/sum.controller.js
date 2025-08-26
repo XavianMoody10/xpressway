239699df1be20cb1ce1b18dc46375b4fd9a0a22c
Xavian Moody <XavianMoody1025@gmail.com>
2025-08-25 20:33:10 -0400

    Removed test folder

--- test/src/controllers/sum.controller.js
+++
@@ -1,6 +0,0 @@
-import { getSum } from "../helpers/sum.helper.js";
-
-export function findTheSumeOfTwoNumbers(req, res) {
-  const sum = getSum(1, 2);
-  res.send(sum);
-}
--- test/src/routes/sum.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { findTheSumeOfTwoNumbers } from "../controllers/sum.controller.js";
-
-const router = Router();
-
-router.get("/", findTheSumeOfTwoNumbers);
-
-export default router;
